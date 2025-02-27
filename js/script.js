315758c7a4dd2d51a0992c0ade0d198502d8601b
Olise-Uwadinmah <oliseuwadinmah@gmail.com>
2025-02-26 17:47:03 -0800

    modified the login page by Oscar the Horse

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ function tglBttn() {
         tglBall.style.left = "21px";
         mP.style.background = "#fff";
         mP.style.color = "#000";
+        tle.style.color = "#000";
     } else {
         tglBtn.style.background = "";
         tglBall.style.transition = "";
@@ -17,7 +18,23 @@ function tglBttn() {
         tglBtn.style.border = "";
         mP.style.background = "";
         tglBall.style.background = "";
-        mP.style.color = ""
+        mP.style.color = "";
+        tle.style.color = "";
     }
 } 
 const mP = document.querySelector(".mainPage");
+let tle = document.querySelector(".headOne");
+let signLink = document.getElementById("linkOne");
+/*
+signLink.addEventListener("focus", function(){
+    window.prompt("Incorrect email address or password!!!");
+}) */ 
+//input code$$ 
+/*
+let userTwoPassword = document.getElementById("user2");
+theValue = userTwoPassword.value;
+if(theValue.length < 8) {
+    window.prompt("Incorrect email address or password!!!");
+}
+*/ 
+const dispCalc = new Array()
